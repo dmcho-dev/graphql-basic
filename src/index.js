34c5c56c5028d8306a743e7af1fa10760df93ffb
Dmcho <dmcho.dev@gmail.com>
2020-02-22 21:35:06 -0800

    done - 28. Deleting Data with Mutations: Part I

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga';
 import uuidv4 from 'uuid/v4'
-import uuid from 'uuid';
 
-// 27. The Input Type
+// 28. Deleting Data with Mutations: Part I
 
 
 // Demo user data
-const users = [
+let users = [
     {
         id: '1', 
         name: 'Dongmin',
@@ -27,7 +26,7 @@ const users = [
     },
 ]
 
-const posts = [
+let posts = [
     {
         id: '10',
         title: "Graphql 101",
@@ -51,7 +50,7 @@ const posts = [
     },
 ]
 
-const comments = [
+let comments = [
     {
         id: '102',
         author: '3',
@@ -91,6 +90,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
         createComment(data: CreateCommentInput!): Comment!
 
@@ -232,6 +232,26 @@ const resolvers = {
             
             users.push(user)
             return user
+        },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex(user=> user.id === args.id)
+            if(userIndex === -1) {
+                throw new Error('User not found')
+            }
+
+            const deleteUser = users.splice(userIndex, 1)
+            posts = posts.filter(post => {
+                const match = post.author === args.id
+                
+                if(match) {
+                    comments = comments.filter(comment => comment.post !== post.id)
+                }
+
+                return !match
+            })
+            comments = comments.filter(comment => comment.author !== args.id)
+
+            return deleteUser[0]
         },
         createPost(parent, args, ctx, info) {
             const userExists = users.some(user => user.id === args.data.author)
